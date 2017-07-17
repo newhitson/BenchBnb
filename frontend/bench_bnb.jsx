abd0bc766aac0edacbe6c5ba53f08ad466a90caa
Nicholas Whitson <newhitson@gmail.com>
2017-07-17 16:48:08 -0700

    added greeting container

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 // import { signUp, logIn, logOut } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+
+  //testing start
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  //testing end
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
